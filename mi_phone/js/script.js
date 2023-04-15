960bd2655f210249f1360b4d993528ba1f9a18ac
Magdalena Holubova <mholubova@proton.me>
2023-04-15 23:42:10 +0200

    mi phone script update + min

--- mi_phone/js/script.js
+++ mi_phone/js/script.js
@@ -1,44 +1,47 @@
+// brand page reveal hidden content button
 function revealer() {
-  let button = document.getElementById('bpToggle')
+    let button = document.getElementById('bpToggle')
 
-  let toggle = function (element) {
-      element.classList.toggle('is-visible');
-  };
+    let toggle = function (element) {
+        element.classList.toggle('is-visible');
+    };
 
-  document.addEventListener('click', function (event) {
-      if (!event.target.classList.contains('toggle')) return;
-      event.preventDefault();
+    document.addEventListener('click', function (event) {
+        if (!event.target.classList.contains('toggle')) return;
+        event.preventDefault();
 
-      let content = document.querySelector(event.target.hash);
-      if (!content) return;
+        let content = document.querySelector(event.target.hash);
+        if (!content) return;
 
-      toggle(content);
+        toggle(content);
 
-  }, false);
+    }, false);
 
-  button.addEventListener('click', function handleClick(){
-      let initialText = document.getElementById('bpToggle--text__collapsed').textContent;
+    button.addEventListener('click', function handleClick(){
+        let initialText = document.getElementById('bpToggle__text--collapsed').textContent;
 
-      if (button.textContent.toLocaleLowerCase().includes(initialText.toLowerCase())) {
-          button.textContent = document.getElementById('bpToggle--text__expanded').textContent;
-      } else {
-          button.textContent = initialText;
-      }
-  });
+        if (button.textContent.toLocaleLowerCase().includes(initialText.toLowerCase())) {
+            button.textContent = document.getElementById('bpToggle__text--expanded').textContent;
+        } else {
+            button.textContent = initialText;
+        }
+    });
 }; revealer();
 
-$("#h1cc").insertAfter("#categoryUpperDescription")
+// hide testing template in production - presentation for vendor
+function template() {
+    if (window.location.href.indexOf("testingBP") > -1) {
+        document.getElementById("bpTest").style.display = "block"
+    };
+}; template();
 
-if (window.location.href.indexOf("testingBP") > -1) {
-document.getElementById("bpTest").style.display = "block"
-}
-
+// header menu
 function hamburger() {
-var icon = document.getElementById('bpHamburger'),
-    list = document.getElementById('bpHamburgerList');
+  var icon = document.getElementById('bpHamburger'),
+      list = document.getElementById('bpHamburgerList');
 
-icon.addEventListener('click', function() {
-  this.classList.toggle('open');
-  list.classList.toggle('active')
-})
+  icon.addEventListener('click', function() {
+    this.classList.toggle('open');
+    list.classList.toggle('active')
+  })
 }; hamburger();
